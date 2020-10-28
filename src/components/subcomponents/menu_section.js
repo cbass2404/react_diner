bd29dbf56ab23ef7fff49a1a515bc88c5c9a9658
Cory Bass <cory.bass.07@gmail.com>
2020-10-28 17:29:42 -0500

    instructor is making me get back to classwork stuff

--- src/components/subcomponents/menu_section.js
+++ src/components/subcomponents/menu_section.js
@@ -1,14 +1,29 @@
 import React from "react";
+import HTMLFlipBook from "react-pageflip";
 
 const Menu = (props) => {
   return (
     <div className="menu-wrapper">
-      <div className="left-page-menu">
-        <h1>stuff</h1>
-      </div>
-      <div className="right-page-menu">
-        <h1>stuff</h1>
-      </div>
+      <HTMLFlipBook width={500} height={700}>
+        <div className="menu-page" id="bf-entree">
+          Page 1
+        </div>
+        <div className="menu-page" id="bf-side">
+          Page 2
+        </div>
+        <div className="menu-page" id="lun-entree">
+          Page 3
+        </div>
+        <div className="menu-page" id="lun-side">
+          Page 4
+        </div>
+        <div className="menu-page" id="din-entree">
+          Page 5
+        </div>
+        <div className="menu-page" id="din-side">
+          Page 6
+        </div>
+      </HTMLFlipBook>
     </div>
   );
 };
