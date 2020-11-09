c49e685c984efe4ad5621c288f2eb36a9750a3b1
Cory Bass <cory.bass.07@gmail.com>
2020-11-09 16:14:01 -0600

    menu data formatted and populated to turn pages TODO: setup interface

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,33 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    const menuData = require("../database/menu.json");
+
     this.state = {
       mealType: "",
       entree: "",
       sideOne: "",
       sideTwo: "",
+      bfEntree: menuData.breakfast.entree,
+      bfSide: menuData.breakfast.side,
+      lunEntree: menuData.lunch.entree,
+      lunSide: menuData.lunch.side,
+      dinEntree: menuData.dinner.entree,
+      dinSide: menuData.dinner.side,
     };
   }
 
   render() {
     return (
       <div className="app-wrapper">
-        <Menu />
+        <Menu
+          bfEntree={this.state.bfEntree}
+          bfSide={this.state.bfSide}
+          lunEntree={this.state.lunEntree}
+          lunSide={this.state.lunSide}
+          dinEntree={this.state.dinEntree}
+          dinSide={this.state.dinSide}
+        />
         <Response />
       </div>
     );
--- src/components/subcomponents/menu_section.js
+++ src/components/subcomponents/menu_section.js
@@ -2,26 +2,33 @@ import React from "react";
 import HTMLFlipBook from "react-pageflip";
 
 const Menu = (props) => {
+  const mapMenu = (meal) => {
+    return meal.map((dish) => {
+      return <div key={dish.name}>{`${dish.name}: $${dish.price}`}</div>;
+    });
+  };
+
   return (
     <div className="menu-wrapper">
       <HTMLFlipBook width={500} height={700}>
         <div className="menu-page" id="bf-entree">
-          Page 1
+          <div>Breakfast</div>
+          {mapMenu(props.bfEntree)}
         </div>
         <div className="menu-page" id="bf-side">
-          Page 2
+          Page 2{mapMenu(props.bfSide)}
         </div>
         <div className="menu-page" id="lun-entree">
-          Page 3
+          Page 3{mapMenu(props.lunEntree)}
         </div>
         <div className="menu-page" id="lun-side">
-          Page 4
+          Page 4{mapMenu(props.lunSide)}
         </div>
         <div className="menu-page" id="din-entree">
-          Page 5
+          Page 5{mapMenu(props.dinEntree)}
         </div>
         <div className="menu-page" id="din-side">
-          Page 6
+          Page 6{mapMenu(props.dinSide)}
         </div>
       </HTMLFlipBook>
     </div>
